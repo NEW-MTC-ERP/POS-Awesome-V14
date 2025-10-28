6095eee4a18797c04c4668d3596c9b126789673d
Ahmed-Shihata <asho20002000@gmail.com>
2025-10-28 11:55:15 +0300

    fix init issue

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
 
 __version__ = "4.1.1"
 
 
 def console(*data):
+    # Import frappe locally within the function to avoid module-level import
+    import frappe
     frappe.publish_realtime("toconsole", data, user=frappe.session.user)
